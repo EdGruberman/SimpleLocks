98aaecb631012771fa33c1e1d8ac48e1c64a7d45
EdGruberman <ed@rjump.com>
2013-10-16 23:18:31 -0700

    release 3.4.0

--- src/edgruberman/bukkit/simplelocks/Configuration.java
+++ src/edgruberman/bukkit/simplelocks/Configuration.java
@@ -30,17 +30,17 @@ public final class Configuration {
     private static ConfigurationDefinition defineConfig() {
         final ConfigurationDefinition result = new ConfigurationDefinition();
 
-        final List<ConfigurationInstruction> v3_4_0b3 = result.createInstructions(StandardVersion.parse("3.4.0b3"));
+        final List<ConfigurationInstruction> v3_4_0 = result.createInstructions(StandardVersion.parse("3.4.0"));
 
-        v3_4_0b3.add(PutKeyInstruction.create(StandardPlugin.DEFAULT_LOG_LEVEL.getName(), StandardPlugin.KEY_LOG_LEVEL));
-        v3_4_0b3.add(PutKeyInstruction.create("§1++ LOCKED ++", "title"));
-        v3_4_0b3.add(PutKeyInstruction.create(Arrays.asList("server.user.{0}", "server.group.{0}"), "permissions"));
-        v3_4_0b3.add(PutKeyInstruction.create(true, "auto-alias", "enabled"));
-        v3_4_0b3.add(PutKeyInstruction.create(15, "auto-alias", "length"));
-        v3_4_0b3.add(PutKeyInstruction.create("~", "auto-alias", "prefix"));
-        v3_4_0b3.add(PutKeyInstruction.create(true, "explosion-protection"));
+        v3_4_0.add(PutKeyInstruction.create(StandardPlugin.DEFAULT_LOG_LEVEL.getName(), StandardPlugin.KEY_LOG_LEVEL));
+        v3_4_0.add(PutKeyInstruction.create("§1++ LOCKED ++", "title"));
+        v3_4_0.add(PutKeyInstruction.create(Arrays.asList("server.user.{0}", "server.group.{0}"), "permissions"));
+        v3_4_0.add(PutKeyInstruction.create(true, "auto-alias", "enabled"));
+        v3_4_0.add(PutKeyInstruction.create(15, "auto-alias", "length"));
+        v3_4_0.add(PutKeyInstruction.create("~", "auto-alias", "prefix"));
+        v3_4_0.add(PutKeyInstruction.create(true, "explosion-protection"));
 
-        v3_4_0b3.add(RemoveKeyInstruction.create("version"));
+        v3_4_0.add(RemoveKeyInstruction.create("version"));
 
         return result;
     }
@@ -48,9 +48,9 @@ public final class Configuration {
     private static ConfigurationDefinition defineLanguage() {
         final ConfigurationDefinition result = new ConfigurationDefinition();
 
-        final List<ConfigurationInstruction> v3_4_0b3 = result.createInstructions(StandardVersion.parse("3.4.0b3"));
+        final List<ConfigurationInstruction> v3_4_0 = result.createInstructions(StandardVersion.parse("3.4.0"));
 
-        v3_4_0b3.add(HeaderInstruction.create(
+        v3_4_0.add(HeaderInstruction.create(
                 "\n"
                 + "---- arguments ----\n"
                 + "region-cancel-create: 0 = generated, 1 = region\n"
@@ -79,49 +79,49 @@ public final class Configuration {
                 + "---- patterns ----"
         ));
 
-        v3_4_0b3.add(PutKeyInstruction.create("§", "format-code"));
+        v3_4_0.add(PutKeyInstruction.create("§", "format-code"));
 
-        v3_4_0b3.add(PutKeyInstruction.create("§f-> §7Locks §ccan not be created§7 in region: §f{1}", "region-cancel-create"));
+        v3_4_0.add(PutKeyInstruction.create("§f-> §7Locks §ccan not be created§7 in region: §f{1}", "region-cancel-create"));
 
-        v3_4_0b3.add(PutKeyInstruction.create("§8, §7", "access-delimiter"));
-        v3_4_0b3.add(PutKeyInstruction.create("§f-> §7Lock §caccess denied §8(Access: §7{1}§8)", "denied"));
-        v3_4_0b3.add(PutKeyInstruction.create("§f-> {2,choice,0#§e|1#§2}Lock Access§8: §7{1}{2,choice,0#|1#' §8(§7To add: §b/lock+ §3§oname§8)'}", "describe"));
-        v3_4_0b3.add(PutKeyInstruction.create("§f-> §7Lock §2broken", "break"));
+        v3_4_0.add(PutKeyInstruction.create("§8, §7", "access-delimiter"));
+        v3_4_0.add(PutKeyInstruction.create("§f-> §7Lock §caccess denied §8(Access: §7{1}§8)", "denied"));
+        v3_4_0.add(PutKeyInstruction.create("§f-> {2,choice,0#§e|1#§2}Lock Access§8: §7{1}{2,choice,0#|1#' §8(§7To add: §b/lock+ §3§oname§8)'}", "describe"));
+        v3_4_0.add(PutKeyInstruction.create("§f-> §7Lock §2broken", "break"));
 
-        v3_4_0b3.add(PutKeyInstruction.create("§f-> §7Lock §2access granted§7 explicitly to §f{1}", "grant-success"));
-        v3_4_0b3.add(PutKeyInstruction.create("§f-> §7Lock access §ealready granted§7 explicitly to §f{1}", "grant-already"));
-        v3_4_0b3.add(PutKeyInstruction.create("§f-> §7Lock §caccess full", "grant-full"));
+        v3_4_0.add(PutKeyInstruction.create("§f-> §7Lock §2access granted§7 explicitly to §f{1}", "grant-success"));
+        v3_4_0.add(PutKeyInstruction.create("§f-> §7Lock access §ealready granted§7 explicitly to §f{1}", "grant-already"));
+        v3_4_0.add(PutKeyInstruction.create("§f-> §7Lock §caccess full", "grant-full"));
 
-        v3_4_0b3.add(PutKeyInstruction.create("§f-> §7Lock explicit §2access revoked§7 from §f{1}", "revoke-success"));
-        v3_4_0b3.add(PutKeyInstruction.create("§f-> §7You §ecan not remove§7 your own access to a lock", "revoke-prevent"));
-        v3_4_0b3.add(PutKeyInstruction.create("§f-> §7Lock access §enot explicitly granted §7to §f{1}", "revoke-missing"));
+        v3_4_0.add(PutKeyInstruction.create("§f-> §7Lock explicit §2access revoked§7 from §f{1}", "revoke-success"));
+        v3_4_0.add(PutKeyInstruction.create("§f-> §7You §ecan not remove§7 your own access to a lock", "revoke-prevent"));
+        v3_4_0.add(PutKeyInstruction.create("§f-> §7Lock access §enot explicitly granted §7to §f{1}", "revoke-missing"));
 
-        v3_4_0b3.add(PutKeyInstruction.create("§f-> §7Alias for §f{1} §7is §f{2}", "alias-alias"));
-        v3_4_0b3.add(PutKeyInstruction.create("§f-> §7Player for §f{1} §7is §f{2}", "alias-player"));
-        v3_4_0b3.add(PutKeyInstruction.create("§f-> §7No alias or player found for §f{1}", "alias-none"));
+        v3_4_0.add(PutKeyInstruction.create("§f-> §7Alias for §f{1} §7is §f{2}", "alias-alias"));
+        v3_4_0.add(PutKeyInstruction.create("§f-> §7Player for §f{1} §7is §f{2}", "alias-player"));
+        v3_4_0.add(PutKeyInstruction.create("§f-> §7No alias or player found for §f{1}", "alias-none"));
 
-        v3_4_0b3.add(PutKeyInstruction.create("§f-> §2Reloaded §7{1} plugin", "reload"));
+        v3_4_0.add(PutKeyInstruction.create("§f-> §2Reloaded §7{1} plugin", "reload"));
 
-        v3_4_0b3.add(PutKeyInstruction.create("§f-> §cNo lock§7 identified", "requires-lock"));
+        v3_4_0.add(PutKeyInstruction.create("§f-> §cNo lock§7 identified", "requires-lock"));
 
-        v3_4_0b3.add(PutKeyInstruction.create("§f-> §cOnly {1} §7can use the §b/{2} §7command", "sender-rejected"));
-        v3_4_0b3.add(PutKeyInstruction.create("§c{0}s", "sender-rejected-valid-item"));
-        v3_4_0b3.add(PutKeyInstruction.create("§4,", "sender-rejected-valid-delimiter"));
+        v3_4_0.add(PutKeyInstruction.create("§f-> §cOnly {1} §7can use the §b/{2} §7command", "sender-rejected"));
+        v3_4_0.add(PutKeyInstruction.create("§c{0}s", "sender-rejected-valid-item"));
+        v3_4_0.add(PutKeyInstruction.create("§4,", "sender-rejected-valid-delimiter"));
 
-        v3_4_0b3.add(PutKeyInstruction.create("§f-> §cMissing §7required argument§8: {2}", "argument-missing"));
-        v3_4_0b3.add(PutKeyInstruction.create("§f-> §cUnknown§7 argument for {2}§8: §f{3}", "argument-unknown"));
-        v3_4_0b3.add(PutKeyInstruction.create("§3§o{0}", "argument-syntax-name"));
-        v3_4_0b3.add(PutKeyInstruction.create("§b{0}", "argument-syntax-known-item"));
-        v3_4_0b3.add(PutKeyInstruction.create("§3|", "argument-syntax-known-delimiter"));
-        v3_4_0b3.add(PutKeyInstruction.create("{0}", "argument-syntax-required"));
-        v3_4_0b3.add(PutKeyInstruction.create("§3[{0}§3]", "argument-syntax-optional"));
+        v3_4_0.add(PutKeyInstruction.create("§f-> §cMissing §7required argument§8: {2}", "argument-missing"));
+        v3_4_0.add(PutKeyInstruction.create("§f-> §cUnknown§7 argument for {2}§8: §f{3}", "argument-unknown"));
+        v3_4_0.add(PutKeyInstruction.create("§3§o{0}", "argument-syntax-name"));
+        v3_4_0.add(PutKeyInstruction.create("§b{0}", "argument-syntax-known-item"));
+        v3_4_0.add(PutKeyInstruction.create("§3|", "argument-syntax-known-delimiter"));
+        v3_4_0.add(PutKeyInstruction.create("{0}", "argument-syntax-required"));
+        v3_4_0.add(PutKeyInstruction.create("§3[{0}§3]", "argument-syntax-optional"));
 
-        v3_4_0b3.add(RemoveKeyInstruction.create("grant"));
-        v3_4_0b3.add(RemoveKeyInstruction.create("revoke"));
-        v3_4_0b3.add(RemoveKeyInstruction.create("alias"));
-        v3_4_0b3.add(RemoveKeyInstruction.create("access"));
+        v3_4_0.add(RemoveKeyInstruction.create("grant"));
+        v3_4_0.add(RemoveKeyInstruction.create("revoke"));
+        v3_4_0.add(RemoveKeyInstruction.create("alias"));
+        v3_4_0.add(RemoveKeyInstruction.create("access"));
 
-        v3_4_0b3.add(RemoveKeyInstruction.create("version"));
+        v3_4_0.add(RemoveKeyInstruction.create("version"));
 
         return result;
     }
